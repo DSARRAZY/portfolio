69d1a5bb15343e2bae07d39ea25dce8ba13f8a29
DSARRAZY <d.sarrazy@gmail.com>
2020-07-22 22:19:27 +0200

    count

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,4 @@
-// Wrap every letter in a span
+// ANIMATION
 var textWrapper = document.querySelector('.ml6 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -16,3 +16,33 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
 });
+
+//COUNTER
+
+$.fn.jQuerySimpleCounter = function( options ) {
+    var settings = $.extend({
+        start:  0,
+        end:    100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options );
+
+    var thisElement = $(this);
+
+    $({count: settings.start}).animate({count: settings.end}, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function() {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
+};
+
+$('#number1').jQuerySimpleCounter({end: 12,duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 55,duration: 3000});
+$('#number3').jQuerySimpleCounter({end: 359,duration: 2000});
+$('#number4').jQuerySimpleCounter({end: 246,duration: 2500});
+
