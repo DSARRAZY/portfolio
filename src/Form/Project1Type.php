fdd741eaa60f763db957c7123787460fcb62cc35
DSARRAZY <d.sarrazy@gmail.com>
2020-07-23 10:02:46 +0200

    front formulaire

--- src/Form/Project1Type.php
+++ src/Form/Project1Type.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,11 @@ class Project1Type extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('language')
-            ->add('year')
-            ->add('description')
-            ->add('link')
+            ->add('name', TextType::class, ['label'=> 'Nom du projet :'])
+            ->add('language', TextType::class, ['label'=> 'Langage(s) utilisé(s) :'])
+            ->add('year', IntegerType::class, ['label'=> 'Année du projet :'])
+            ->add('description', TextareaType::class, ['label'=> 'Description :'])
+            ->add('link', TextType::class, ['label'=> 'Lien vers le projet :'])
         ;
     }
 
